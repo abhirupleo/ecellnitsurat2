ea76cb094f13094fbc9c0dac868f512062276e1e
Abhirup Panja <AbhirupLeo@Leomessi.local>
2015-09-18 13:35:13 +0530

    added box shadows and tweaks

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$(window).scroll(function(){
         var y = $(document).scrollTop();
         var menu = $(".menubar"); 
-        if(y>50){
+        if(y>30){
     	menu.addClass("fixed");
     } 
     else{
